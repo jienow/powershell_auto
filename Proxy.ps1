3cfa67ddc26fa66b2a87ac325ba34dee7b2624a0
互联网高级技工 <2609482146@qq.com>
2024-04-12 15:45:31 +0800

    debug

--- Proxy.ps1
+++ Proxy.ps1
@@ -1,14 +1,18 @@
+$global:initStatus = $false
 function setProxy {
-  $Env:http_proxy="http://127.0.0.1:7890";$Env:https_proxy="http://127.0.0.1:7890"
-  getProxy
+  $Env:http_proxy="http://127.0.0.1:7890";$Env:https_proxy="http://127.0.0.1:7890";
+  if ($global:initStatus -eq $true){
+    getProxy
+  }
+  $global:initStatus = $true
 }
 function unsetProxy {
-  $Env:http_proxy=$null
-  $Env:https_proxy=$null
-  getProxy
+  $Env:http_proxy=$null;
+  $Env:https_proxy=$null;
+  getProxy;
 }
 function getProxy {
-  $Env:http_proxy
+  $Env:http_proxy;
   $Env:https_proxy
 }
 
--- Vm.ps1
+++ Vm.ps1
@@ -45,5 +45,3 @@ function sshArch {
 function GuaQiArch {
   & vmrun suspend $vmPath
 }
-
-ArchStatus
