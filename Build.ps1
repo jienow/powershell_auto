9e9569393ea4816e7e5117084b99c675cedc5c7a
互联网高级技工 <2609482146@qq.com>
2024-04-15 14:14:03 +0800

    nvim alias

--- Build.ps1
+++ Build.ps1
@@ -1,17 +1,23 @@
-function cb {
+function cb
+{
   cmake -B build;
-  if (!$?){
-    Write-Host "cmake构建失败"
+  if (!$?)
+  {
+    Write-Host "cmake构建失败";
     return;
   }
+  Write-Host  -ForegroundColor Cyan "------------------------------------------------构建成功";
   cmake --build .\build\;
-  if (!$?){
-    Write-Host "编译失败"
+  if (!$?)
+  {
+    Write-Host "编译失败";
     return;
   }
-  ./build/main.exe
-  if (!$?){
-    Write-Host "main执行失败，可执行文件名可能不是main.exe"
+  Write-Host  -ForegroundColor Green "------------------------------------------------编译成功";
+  ./build/main.exe;
+  if (!$?)
+  {
+    Write-Host "main执行失败，可执行文件名可能不是main.exe";
     return;
   }
 }
---
+++ Config.ps1
@@ -0,0 +1,1 @@
+Set-Alias -Name v -Value "D:\scoop\apps\neovim\0.9.5\bin\nvim.exe"
