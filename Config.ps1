e85d68f9f79dd397a18475c4877685dedfc8cf64
互联网高级技工 <2609482146@qq.com>
2025-03-12 22:28:50 +0800

    nvimhome

--- Config.ps1
+++ Config.ps1
@@ -1,3 +1,4 @@
 Set-Alias -Name v -Value "D:\scoop\apps\neovim\current\bin\nvim.exe"
 # sdfdf
 Set-Alias -Name y -Value "D:\scoop\apps\yazi\current\yazi.exe"
+$NvimHome = 'C:\Users\26094\AppData\Local\nvim'
